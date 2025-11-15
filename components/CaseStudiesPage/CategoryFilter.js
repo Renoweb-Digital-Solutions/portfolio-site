3c0dc190205e46ff192fc70aefa1f644cd70b3cf
Samaresh-Das <samareshmail679@gmail.com>
2025-11-15 19:31:07 +0530

    Removed some case studies and added some more filter

--- components/CaseStudiesPage/CategoryFilter.js
+++ components/CaseStudiesPage/CategoryFilter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CategoryFilter = ({ activeCategory, setActiveCategory }) => {
-    const categories = ['All', 'Lead Gen', 'SMM OS', 'Organic', 'Performance OS', 'LinkedIn', 'Community Management']
+    const categories = ['All', 'Lead Gen', 'SMM OS', 'Organic', 'Performance OS', 'LinkedIn', 'Community Management', "Dev OS"]
 
     return (
         <div>
--- components/data/case_studies.js
+++ components/data/case_studies.js
@@ -1,33 +1,5 @@
 export const case_studies_data = [
-  {
-    id: "lead-gen-canadian-saas",
-    category: "Lead Gen",
-    title: "Lead Generation Funnel for Canadian SaaS Company",
-    about_client: "Our client, a Canadian SaaS company, was struggling to generate sufficient B2B leads for their analytics dashboard solution. Despite their best efforts, they were only getting 2-3 demo requests per month.",
-    challenges: ["The client needed a lead generation funnel that could effectively target potential customers, capture their interest, and qualify them for demos."],
-    solutions: {
-      approach: "The client needed a lead generation funnel that could effectively target potential customers, capture their interest, and qualify them for demos.",
 
-      process: ["Capture interest through targeted posts and lead magnets", "Qualify leads through a series of pertinent questions", "Nurture qualified leads through an email cadence that highlighted the value of the product"]
-    },
-    conclusion: "The lead generation funnel we designed and implemented was a resounding success. By targeting the right audience, capturing their interest, and qualifying them for demos, we were able to increase demo requests by 18-19X in just three months. The client was thrilled with the results and had to hire more client success managers to handle the increased demand.",
-
-    takeaway: "A well-designed lead generation funnel can make all the difference in driving business growth. By understanding your target audience, creating targeted content, and qualifying leads, you can increase conversions and drive revenue."
-  },
-  {
-    id: "lead-gen-healthcare-us",
-    category: "Lead Gen",
-    title: "Omni-channel Lead Generation for US Healthcare Group",
-    about_client: "Our client, a US-based healthcare group, was struggling to attract new patients and reduce no-shows for appointments. They needed a comprehensive lead generation strategy to drive growth and revenue.",
-    challenges: ["Low online visibility and website traffic", "Insufficient lead generation and appointment bookings", "High no-show rates for appointments"],
-    solutions: {
-      approach: "We designed and implemented an omnichannel lead generation funnel that included:",
-
-      process: ["Local SEO optimization to increase online visibility", "Landing pages to capture leads and book appointments", "Email cadence to nurture leads and reduce no-shows", "Local ads to drive traffic and bookings", "Social media lead ads and funnel to capture leads", "Automation workflow to streamline lead follow-up and appointment reminders"]
-
-    },
-    conclusion: "Our omnichannel lead generation strategy helped the US healthcare group drive significant growth and revenue. By leveraging local SEO, email marketing, and automation workflows, we reduced no-show rates and increased appointment bookings. This case study demonstrates the effectiveness of a comprehensive lead generation strategy in driving business success."
-  },
   {
     "id": "lead-gen-edtech-daily-demos",
     "category": "Lead Gen",
