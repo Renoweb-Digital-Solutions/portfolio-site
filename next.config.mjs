3af54f47242581ea49d6b5f90de502faec8c77ba
Samaresh-Das <samareshmail679@gmail.com>
2026-05-25 16:47:55 +0530

    feat: add ContactForm component and remove internal API rewrite configuration

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,6 @@ const nextConfig = {
     images: {
         domains: ['flowbite.com'],
     },
-    async rewrites() {
-        return [
-            {
-                source: '/api/contact',
-                destination: 'http://localhost:5004/api/contact',
-            },
-        ];
-    },
 };
 
 export default nextConfig;
